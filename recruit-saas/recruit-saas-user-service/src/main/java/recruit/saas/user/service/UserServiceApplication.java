4c46ed027a9989a5c4f16096e1be32e22fa8d291
zhangshenao <zhangshenao@kkworld.com>
2023-02-18 17:22:27 +0800

    Add Nacos Discovery Client

--- recruit-saas/recruit-saas-user-service/src/main/java/recruit/saas/user/service/UserServiceApplication.java
+++ recruit-saas/recruit-saas-user-service/src/main/java/recruit/saas/user/service/UserServiceApplication.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author ZhangShenao
@@ -13,6 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @Slf4j
 @MapperScan(basePackages = "recruit.saas.user.service.mapper")  //开启MyBatis Mapper扫描
+@EnableDiscoveryClient  //开启SpringCloud服务注册与发现客户端
 public class UserServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
